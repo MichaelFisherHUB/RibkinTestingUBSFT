500b7b398005d5f61cac7c258973cd536875a3d9
MichaelFisherHUB <51174839+MichaelFisherHUB@users.noreply.github.com>
2019-08-03 17:41:48 +0300

    Base logic. Health controll and gravity emitter.

--- Assets/Scripts/Base/GravityEmitter.cs
+++ Assets/Scripts/Base/GravityEmitter.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
-[SerializeField]
-public class GravityEmitter
+[System.Serializable]
+public struct GravityEmitter
 {
     [SerializeField]
     private float _mass;
@@ -11,5 +11,19 @@ public class GravityEmitter
         {
             return _mass > 0 ? _mass : Mathf.Abs(_mass);
         }
+        private set
+        {
+            _mass = value;
+        }
+    }
+
+    public void SetRandomMass()
+    {
+        SetRandomMass(1000f, 10000f);
+    }
+
+    public void SetRandomMass(float fromValue, float toValue)
+    {
+        Mass = Random.Range(fromValue, toValue);
     }
 }
---
+++ Assets/Scripts/Base/SolarSystemBodies/SolarSystemBodyBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystemBodyBase : MonoBehaviour
+{
+    public GravityEmitter gravityEmitter = new GravityEmitter();
+    public HealthControll healthControll = new HealthControll();
+
+    private void Awake()
+    {
+        healthControll.AddListeners(HealthChangeHandler, OnDieHandler);
+    }
+
+    public virtual void HealthChangeHandler(int newHealth)
+    {
+
+    }
+
+    public virtual void OnDieHandler()
+    {
+
+    }
+}
