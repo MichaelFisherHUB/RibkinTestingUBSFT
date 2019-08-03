29444f05568e0522b249e2d739a61f08e39bf874
MichaelFisherHUB <51174839+MichaelFisherHUB@users.noreply.github.com>
2019-08-03 15:23:53 +0300

    Add basics

---
+++ Assets/Scripts/Base/GravityEmitter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[SerializeField]
+public class GravityEmitter
+{
+    [SerializeField]
+    private float _mass;
+    public float Mass
+    {
+        get
+        {
+            return _mass > 0 ? _mass : Mathf.Abs(_mass);
+        }
+    }
+}
