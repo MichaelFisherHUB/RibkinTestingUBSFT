82200fcdd468b9b7d9ae67fbdb7bd314aef7faba
MichaelFisherHUB <51174839+MichaelFisherHUB@users.noreply.github.com>
2019-08-03 18:03:05 +0300

    Some changes with base classes

--- Assets/Scripts/Base/SolarSystemBodies/SolarSystemBodyBase.cs
+++ Assets/Scripts/Base/SolarSystemBodies/SolarSystemBodyBase.cs
@@ -2,23 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SolarSystemBodyBase : MonoBehaviour
+public class SolarSystemBodyBase : MonoBehaviour, ITakeDamagable, IDeadable
 {
     public GravityEmitter gravityEmitter = new GravityEmitter();
-    public HealthControll healthControll = new HealthControll();
 
-    private void Awake()
+    [SerializeField]
+    private int _health = 100;
+    public int Health
     {
-        healthControll.AddListeners(HealthChangeHandler, OnDieHandler);
+        get
+        {
+            return _health;
+        }
+
+        private set
+        {
+            _health = value;
+            if (_health <= 0)
+            {
+                _health = 0;
+                if (onHealthValueChange != null)
+                {
+                    onHealthValueChange.Invoke(Health);
+                }
+                Die();
+            }
+        }
     }
 
-    public virtual void HealthChangeHandler(int newHealth)
-    {
+    private System.Action<int> onHealthValueChange;
+    private System.Action onDieAction;
 
+    #region Constructors
+
+    public SolarSystemBodyBase(int startHealth = 0)
+    {
+        if (startHealth != 0)
+        {
+            _health = startHealth;
+        }
     }
 
-    public virtual void OnDieHandler()
+    public SolarSystemBodyBase(System.Action<int> onHealthChange, System.Action onDie, int startHealth = 0)
     {
-
+        onHealthValueChange += onHealthChange;
+        onDieAction += onDie;
+        if (startHealth != 0)
+        {
+            _health = startHealth;
+        }
     }
+    #endregion
+
+    #region AddListeners
+
+    public void AddListeners(System.Action<int> onHealthChange, System.Action onDieHeandler)
+    {
+        AddHealthListener(onHealthChange);
+        AddDeadListener(onDieHeandler);
+    }
+
+    public void AddHealthListener(System.Action<int> onHealthChange)
+    {
+        if (onHealthChange != null)
+        {
+            onHealthValueChange += onHealthChange;
+        }
+    }
+
+    public void AddDeadListener(System.Action onDieHeandler)
+    {
+        if (onDieHeandler != null)
+        {
+            onDieAction += onDieHeandler;
+        }
+    }
+    #endregion
+
+    #region interfaces
+
+    public virtual void TakeDamage(int damageValue)
+    {
+        Health -= damageValue;
+    }
+
+    public void Die()
+    {
+        if (onDieAction != null)
+        {
+            onDieAction.Invoke();
+        }
+    }
+    #endregion
 }
---
+++ Assets/Scripts/Testing/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public SolarSystemBodyBase solar;
+
+    [ContextMenu("Testing")]
+	public void Testing()
+    {
+        ITakeDamagable damageInterface = solar.GetComponent<ITakeDamagable>();
+        Debug.Log(damageInterface == null ? "NUll" : "There");
+    }
+}
